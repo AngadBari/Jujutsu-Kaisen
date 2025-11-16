3e71317d468f30869eb1372755cd53a48661288b
AngadBari <angadbari2@gmail.com>
2025-11-16 16:57:37 +0530

    Initial project upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Nav from './compontes/nav/Nav'
-import Character from './Compontes/Character/Character'
-import Power from './Compontes/Power/Power'
+import Character from './compontes/character/Character'
+import Power from './compontes/power/Power'
 import Top from './compontes/top/top'
+
 function App() {
   return (
       <>
       <Nav />
-      <Top />
+     <Top/>
       <Character />
       <Power />
       </>
