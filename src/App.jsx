d710335ff48e47679edf2a79774cc0e381555125
AngadBari <angadbari2@gmail.com>
2025-11-16 14:01:02 +0530

    cool

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import Nav from './compontes/Nav/Nav'
-import Top from './compontes/top/top'
 import Character from './Compontes/Character/Character'
 import Power from './Compontes/Power/Power'
-
+import Top from './compontes/top/top'
 function App() {
   return (
       <>
